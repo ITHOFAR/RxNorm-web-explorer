5807a5a325cd146e9173e7db1d674b0f6b5d5bbe
ITHOFAR <isaac.t.harris@gmail.com>
2023-03-19 12:52:58 -0400

    corrected cims scripts calling on wrong directory

--- db/scripts/cims-scripts/load-pg.sh
+++ db/scripts/cims-scripts/load-pg.sh
@@ -8,9 +8,9 @@ cd "$SCRIPT_DIR"/..
 # creating rxnorm tables
 psql -U rxnorm -f sql/rxnorm-orig/RxNormDDL.sql -v ON_ERROR_STOP=1
 # populates created rxnorm tables
-"$SCRIPT_DIR"/cims-scripts/populate-rxnorm-tables.sh
+"$SCRIPT_DIR"/populate-rxnorm-tables.sh
 
 # creating derived tables from rxnorm tables
 psql -U rxnorm -f sql/create-derived-tables.sql -v ON_ERROR_STOP=1
 # populating derived tables
-psql -U rxnorm -f "$SCRIPT_DIR"/cims-scripts/populate-derived-tables-cims.sql -v ON_ERROR_STOP=1
+psql -U rxnorm -f "$SCRIPT_DIR"/populate-derived-tables-cims.sql -v ON_ERROR_STOP=1
