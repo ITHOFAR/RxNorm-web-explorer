6af3eb4ba4ad67e1c1940131af5801756d1d4422
ITHOFAR <isaac.t.harris@gmail.com>
2023-03-19 15:27:35 -0400

    corrected cims scripts over wrong file location

--- db/scripts/cims-scripts/populate-rxnorm-tables.sh
+++ db/scripts/cims-scripts/populate-rxnorm-tables.sh
@@ -5,37 +5,37 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 cd "$SCRIPT_DIR"/..
 
 echo Loading rxnconso
-sed 's/|$//' sql/rxnorm-orig/rrf/RXNCONSO.RRF |
+sed 's/|$//' "$SCRIPT_DIR"/../../sql/rxnorm-orig/rrf/RXNCONSO.RRF |
   psql -U rxnorm -c "\copy rxnconso from stdin with delimiter '|' csv quote E'\b' null as ''"
 
 echo Loading rxnsat
-sed 's/|$//' sql/rxnorm-orig/rrf/RXNSAT.RRF |
+sed 's/|$//' "$SCRIPT_DIR"/../../sql/rxnorm-orig/rrf/RXNSAT.RRF |
   psql -U rxnorm -c "\copy rxnsat from stdin with delimiter '|' csv quote E'\b' null as ''"
 
 echo Loading rxnrel
-sed 's/|$//' sql/rxnorm-orig/rrf/RXNREL.RRF |
+sed 's/|$//' "$SCRIPT_DIR"/../../sql/rxnorm-orig/rrf/RXNREL.RRF |
   psql -U rxnorm -c "\copy rxnrel from stdin with delimiter '|' csv quote E'\b' null as ''"
 
 echo Loading rxndoc
-sed 's/|$//' sql/rxnorm-orig/rrf/RXNDOC.RRF |
+sed 's/|$//' "$SCRIPT_DIR"/../../sql/rxnorm-orig/rrf/RXNDOC.RRF |
   psql -U rxnorm -c "\copy rxndoc from stdin with delimiter '|' csv quote E'\b' null as ''"
 
 echo Loading rxnsty
-sed 's/|$//' sql/rxnorm-orig/rrf/RXNSTY.RRF |
+sed 's/|$//' "$SCRIPT_DIR"/../../sql/rxnorm-orig/rrf/RXNSTY.RRF |
   psql -U rxnorm -c "\copy rxnsty from stdin with delimiter '|' csv quote E'\b' null as ''"
 
 echo Loading rxnsab
-sed 's/|$//' sql/rxnorm-orig/rrf/RXNSAB.RRF |
+sed 's/|$//' "$SCRIPT_DIR"/../../sql/rxnorm-orig/rrf/RXNSAB.RRF |
   psql -U rxnorm -c "\copy rxnsab from stdin with delimiter '|' csv quote E'\b' null as ''"
 
 echo Loading rxnatomarchive
-sed 's/|$//' sql/rxnorm-orig/rrf/RXNATOMARCHIVE.RRF |
+sed 's/|$//' "$SCRIPT_DIR"/../../sql/rxnorm-orig/rrf/RXNATOMARCHIVE.RRF |
   psql -U rxnorm -c "\copy rxnatomarchive from stdin with delimiter '|' csv quote E'\b' null as ''"
 
 echo Loading rxncuichanges
-sed 's/|$//' sql/rxnorm-orig/rrf/RXNCUICHANGES.RRF |
+sed 's/|$//' "$SCRIPT_DIR"/../../sql/rxnorm-orig/rrf/RXNCUICHANGES.RRF |
   psql -U rxnorm -c "\copy rxncuichanges from stdin with delimiter '|' csv quote E'\b' null as ''"
 
 echo Loading rxncui
-sed 's/|$//' sql/rxnorm-orig/rrf/RXNCUI.RRF |
+sed 's/|$//' "$SCRIPT_DIR"/../../sql/rxnorm-orig/rrf/RXNCUI.RRF |
   psql -U rxnorm -c "\copy rxncui from stdin with delimiter '|' csv quote E'\b' null as ''"
--- db/sql/rxnorm-orig/README.md
+++ db/sql/rxnorm-orig/README.md
@@ -2,3 +2,5 @@
 
 Removed drop table lines from RxNormDDL.sql to simplfy Dockerfile use of scripts.
 Combined both index files into RxNormDDL
+
+## Place rxnorm files inside rrf folder
